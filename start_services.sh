a8892ed1873daf564862b83256663215cfde32f3
Paladin Minna <paminna@si-b3.kzn.21-school.ru>
2021-05-14 17:39:54 +0300

    php and wordpress started

--- start_services.sh
+++ start_services.sh
@@ -2,10 +2,19 @@
 
 minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
-docker build -t nginx-image .
-kubectl delete pods
-kubectl apply -f nginx.yaml
+docker build -t nginx-image ./srcs/nginx
 minikube addons enable metallb
+# kubectl delete pods
 
+# nginx start
+kubectl apply -f ./srcs/nginx/config_map.yaml
+kubectl apply -f ./srcs/nginx/nginx.yaml
+docker pull metallb/speaker:v0.8.2
+docker pull metallb/controller:v0.8.2
+
+# wordpress start
+
+
+minikube dashboard
 
 sh
