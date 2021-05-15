29c0e50ef85163da25afd490333962aa566fcbde
Paladin Minna <paminna@si-c5.kzn.21-school.ru>
2021-05-15 19:10:13 +0300

    php started

---
+++ srcs/wordpress/wp-sample_config.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Основные параметры WordPress.
+ *
+ * Скрипт для создания wp-config.php использует этот файл в процессе
+ * установки. Необязательно использовать веб-интерфейс, можно
+ * скопировать файл в "wp-config.php" и заполнить значения вручную.
+ *
+ * Этот файл содержит следующие параметры:
+ *
+ * * Настройки MySQL
+ * * Секретные ключи
+ * * Префикс таблиц базы данных
+ * * ABSPATH
+ *
+ * @link https://ru.wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
+/** Имя базы данных для WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** Имя пользователя MySQL */
+define( 'DB_USER', 'user' );
+
+/** Пароль к базе данных MySQL */
+define( 'DB_PASSWORD', 'password' );
+
+/** Имя сервера MySQL */
+define( 'DB_HOST', 'localhost' );
+
+/** Кодировка базы данных для создания таблиц. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** Схема сопоставления. Не меняйте, если не уверены. */
+define( 'DB_COLLATE', '' );
+
+define( 'AUTH_KEY',         ',W-jjwZi,@Is?7hs1gl=3LhPTfh&;$m5' );
+define( 'SECURE_AUTH_KEY',  '6N9%W9l8rE№.1;G76eBD73jjm!i+%!p3' );
+define( 'LOGGED_IN_KEY',    '&I1ROFOhME=1r2s:4KNLVxf=^UgjyX__' );
+define( 'NONCE_KEY',        '#T=2,D,H*Rvb62E3SK=s38;9*?gMdrXm' );
+define( 'AUTH_SALT',        'Gs&of&oL*96eKB6R7PWJ,bgysm!!!TP?' );
+define( 'SECURE_AUTH_SALT', 'g#svSp|IsNRH=eN3A|i=1C63l$&s&XB9' );
+define( 'LOGGED_IN_SALT',   'gs#s1aey,tdAF9?xHjma:175|LoIq16,' );
+define( 'NONCE_SALT',       '5;an_D530m:LCbU99./.@D|z,n=z!Q!5' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
--- start_services.sh
+++ start_services.sh
@@ -3,12 +3,14 @@
 minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
 docker build -t nginx-image ./srcs/nginx
+docker build -t php-image ./srcs/php
 minikube addons enable metallb
 # kubectl delete pods
 
 # nginx start
 kubectl apply -f ./srcs/nginx/config_map.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/php/php.yaml
 docker pull metallb/speaker:v0.8.2
 docker pull metallb/controller:v0.8.2
 
