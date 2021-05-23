655cdcc50b99fc6bd923f02278c38ed78a6625f8
Paladin Minna <paminna@pr-c1.kzn.21-school.ru>
2021-05-23 16:14:20 +0300

    sql started, php and wp checked

--- start_services.sh
+++ start_services.sh
@@ -4,7 +4,8 @@ minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
 docker build -t nginx-image ./srcs/nginx
 docker build -t php-image ./srcs/php
-docker build -t php-image ./srcs/wordpress
+docker build -t wordpress-image ./srcs/wordpress
+docker build -t mysql-image ./srcs/mysql
 minikube addons enable metallb
 # kubectl delete pods
 
@@ -13,6 +14,7 @@ kubectl apply -f ./srcs/nginx/config_map.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/php/php.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
 docker pull metallb/speaker:v0.8.2
 docker pull metallb/controller:v0.8.2
 
