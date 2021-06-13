030f24ca85c93ff79fe80c096205f8e4507e9094
Paladin Minna <paminna@ge-m5.kzn.21-school.ru>
2021-06-13 18:01:30 +0300

    sql working

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 rc default
-/etc/init.d/mysql setup
-rc-service mysql start
+/etc/init.d/mariadb setup
+rc-service mariadb start
 
 mysql -e "CREATE DATABASE Wordpress;"
 mysql -e "CREATE USER 'user'@'%' IDENTIFIED BY '123';"
 mysql -e "GRANT ALL PRIVILEGES ON Wordpress.* TO 'user'@'%' IDENTIFIED BY '123';"
 mysql -e "FLUSH PRIVILEGES;"
-
 # mysql wordpress < /home/wordpress.sql
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+rc-service mariadb stop
 
-sh
+/usr/bin/supervisord -c /etc/supervisord.conf
